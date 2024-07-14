ffbc493cc5f30729c3af1e7c26d4345a83031127
Avishek Dangol <dangolavishek@gmail.com>
2024-07-14 16:15:13 +0545

    minor changes

--- src/utils/disableScrollPlugin.ts
+++ src/utils/disableScrollPlugin.ts
@@ -3,7 +3,9 @@ import { ScrollbarPlugin } from 'smooth-scrollbar'
 class DisableScroll extends ScrollbarPlugin {
   static pluginName = 'DisableScroll'
 
-  transformDelta(delta, fromEvent) {
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars, @typescript-eslint/no-unused-vars
+  transformDelta(delta:any, _fromEvent:any) {
+    // eslint-disable-next-line no-param-reassign
     delta.x = 0
 
     return delta
