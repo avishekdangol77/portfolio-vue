80ba43d85b308b37b45cf3c7bd5aa518a85626c8
Avishek Dangol <dangolavishek@gmail.com>
2025-08-24 10:22:44 +0100

    cookieless analytics tracking

--- src/composables/usePostHog.ts
+++ src/composables/usePostHog.ts
@@ -4,7 +4,8 @@ const usePostHog = () => {
   posthog.init('phc_HWZOmnrXGL5hk7NYY1WOziyW4AIF1JxIfo2lGw83bXK', {
     api_host: 'https://eu.i.posthog.com',
     defaults: '2025-05-24',
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'always',
+    persistence: 'localStorage'
   })
 
   return { posthog }
